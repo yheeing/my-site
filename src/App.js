d5ac425a36cbdf93dd984df07d6c5693ab517f87
yheeing <yheeing@tvstorm.com>
2020-06-04 14:02:22 +0900

    Added material-ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
+import Breadcrumbs from '@material-ui/core/Breadcrumbs';
+import { emphasize, withStyles } from '@material-ui/core/styles';
+import Chip from '@material-ui/core/Chip';
+import HomeIcon from '@material-ui/icons/Home';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+// import Link from '@material-ui/core/Link';
+// import ProTip from './ProTip';
+
+
+const StyledBreadcrumb = withStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.grey[100],
+    height: theme.spacing(3),
+    color: theme.palette.grey[800],
+    fontWeight: theme.typography.fontWeightRegular,
+    '&:hover, &:focus': {
+      backgroundColor: theme.palette.grey[300],
+    },
+    '&:active': {
+      boxShadow: theme.shadows[1],
+      backgroundColor: emphasize(theme.palette.grey[300], 0.12),
+    },
+  },
+}))(Chip); // TypeScript only: need a type cast here because https://github.com/Microsoft/TypeScript/issues/26591
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <Container maxWidth="sm">
+      {/* <Box my={4}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Create React App v4-beta example
+        </Typography>
+        <ProTip />
+        <Copyright />
+      </Box> */}
+
+      <Breadcrumbs aria-label="breadcrumb">
+      <StyledBreadcrumb
+        component="a"
+        href="#"
+        label="Home"
+        icon={<HomeIcon fontSize="small" />}
+        // onClick={handleClick} TODO:
+      />
+      <StyledBreadcrumb component="a" href="#" label="About" /*onClick={handleClick} TODO: */ />
+      <StyledBreadcrumb
+        label="Contact"
+        deleteIcon={<ExpandMoreIcon />}
+        // onClick={handleClick} TODO:
+        // onDelete={handleClick} TODO:
+      />
+    </Breadcrumbs>
+
+
+    </Container>
   );
 }
 
