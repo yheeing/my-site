c6fc929392edf92f88a125b15a1295242eb9eeae
yheeing <yheeing@tvstorm.com>
2020-06-05 14:33:56 +0900

    Updates

---
+++ precache-manifest.56971f14d1fdd0e55a6e6f4fa8266e49.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbb9b922fd99bc32eb4c2d92070ad5a1",
+    "url": "/my-site/index.html"
+  },
+  {
+    "revision": "f93d8e582eece00d3fe0",
+    "url": "/my-site/static/js/2.7429f717.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/my-site/static/js/2.7429f717.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02a1c653b2e0fb7f691d",
+    "url": "/my-site/static/js/main.545ecff6.chunk.js"
+  },
+  {
+    "revision": "b208088627a69cae8925",
+    "url": "/my-site/static/js/runtime-main.72ed8bde.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-site/precache-manifest.306bf4e849ad1ad38ae123631b13e9b9.js"
+  "/my-site/precache-manifest.56971f14d1fdd0e55a6e6f4fa8266e49.js"
 );
 
 self.addEventListener('message', (event) => {
